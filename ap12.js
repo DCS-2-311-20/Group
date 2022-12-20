c873c21e4ad26def34c719860686ef2204c86f3a
Takashi MINOHARA <minohara@cs.takushoku-u.ac.jp>
2022-12-20 23:53:49 +0900

    初期コード

--- ap12.js
+++ ap12.js
@@ -13,98 +13,101 @@ import * as L4 from "./ap12L4.js";
 
 // ３Ｄページ作成関数の定義
 function init() {
-  // 制御変数の定義
-  
+  // シーンの構成を4つの部分に分けて分担する
   const parts = [
     {module: L1, id: "left1", offset: {x: 50, z:-40}},
     {module: L2, id: "left2", offset: {x:-50, z:-40}},
     {module: L3, id: "left3", offset: {x:-50, z: 40}},
     {module: L4, id: "left4", offset: {x: 50, z: 40}},
   ];
-  
+  // 画面を 2:8 に分割する
   let sizeR = 0.8 * window.innerWidth;
   let sizeL = 0.2 * window.innerWidth;
   // シーン作成
   const scene = new THREE.Scene();
-
    // カメラの作成
   const mainCamera = new THREE.PerspectiveCamera(60, 1, 0.1, 1000);
   {
     mainCamera.position.set(50,50,200);
     mainCamera.lookAt(0,0,0);
   }
-
-  // レンダラ
+  // メインレンダラ
   const mainRenderer = new THREE.WebGLRenderer();
   {
     mainRenderer.setClearColor(0x204060);
     mainRenderer.setPixelRatio(window.devicePixelRatio);
     mainRenderer.setSize(sizeR, sizeR);
   }
-
+  document.getElementById("main").appendChild(mainRenderer.domElement);
   // 3Dモデル(GLTF形式)の読み込み
-
-  const PREFIX = "SHTV_Prefab_Car_"
-  const loader = new GLTFLoader();
+  const PREFIX = "SHTV_Prefab_Car_"; // 4つの車の接頭辞
+  const modelLoader = new GLTFLoader();
+  const textureLoader = new THREE.TextureLoader();
   let cars = [];
-  loader.load("cars/scene.gltf", model => {
-    model.scene.traverse( obj => {
-      obj.rotation.y = 0;
-      if (obj.isMesh) {
-        obj.castShadow = true;
-        obj.material.needsUpdate = true;
-        if ( obj.name.indexOf("Wheel_Ambulance_FR") == 11 
-          || obj.name.indexOf("Wheel_Ambulance_BR") == 11
-        ) {
-          obj.geometry.translate(-200, 0, 0)
+  textureLoader.load("cityTexture.png", texture => {
+    modelLoader.load("cars/scene.gltf", model => {
+      model.scene.traverse(obj => {
+        // 車の向きをリセットする
+        obj.rotation.y = 0;
+        if (obj.isMesh) {
+          obj.castShadow = true;
+          obj.material.needsUpdate = true;
+          // 車の部品の中心をずらす (救急車の車輪だけ特別扱い)
+          if (obj.name.indexOf("Wheel_Ambulance_FR") == 11
+            || obj.name.indexOf("Wheel_Ambulance_BR") == 11
+          ) {
+            obj.geometry.translate(-200, 0, 0)
+          }
+          else {
+            obj.geometry.translate(200, 0, 0)
+          }
         }
-        else {
-          obj.geometry.translate(200, 0, 0)
+        if (obj.name.indexOf(PREFIX) == 0) {
+          cars.push(obj); // 車を1台ずつ配列に入れる
         }
-      }
-      if (obj.name.indexOf(PREFIX) == 0) {
-        cars.push(obj);
-      }
-    });
+      });
 
-    parts.forEach((part, i) => {
-      part.module.init(scene, sizeL, part.id, part.offset);
+      // 分担部分の初期化
+      parts.forEach((part) => {
+        part.module.init(scene, sizeL, part.id, part.offset, texture);
+      });
+      // 分担部分の自動運転コースの作成
+      parts.forEach((part) => {
+        part.module.makeCourse(scene);
+      });
+      // 分担部分の描画
+      parts.forEach((part, i) => {
+        part.module.setCar(scene, cars[i]);
+      });
+      // 全体の描画の開始
+      render();
     });
-    L1.makeCourse(scene);
-    L2.makeCourse(scene);
-    L3.makeCourse(scene);
-    L4.makeCourse(scene);
-    parts.forEach((part, i) => {      
-      part.module.setCar(scene, cars[i]);
-      part.module.render(scene, cars[i]);
-    });
-    render();
   });
-
   // 光源の設定
   { // 環境ライト
     const light = new THREE.AmbientLight();
     light.intensity=0.4;
     scene.add(light);
   }
-  
-  // レンダラーの配置
-  document.getElementById("main").appendChild(mainRenderer.domElement);
-  
-   // Windowサイズの変更処理
+  { // ポイントライト
+    const light = new THREE.PointLight();
+    light.position.set(0, 50, 0);
+    scene.add(light);
+  }
+  // Windowサイズの変更処理
   window.addEventListener("resize", ()=>{
     mainCamera.updateProjectionMatrix();
     sizeR = 0.8 * window.innerWidth;
     mainRenderer.setSize(sizeR, sizeR);
   }, false);
-
+  // 各セクションをクリックしたときにメイン表示のカメラを切り替える
   let camera = mainCamera;
   parts.forEach((part) => {
     document.getElementById(part.id).onclick = () => {
       camera = part.module.getCamera();
     }
   });
-
+  // メイン部分をクリックしたときにカメラを元に戻す
   document.getElementById("main").onclick = () => {
     camera = mainCamera;
   }
--- ap12L3.js
+++ ap12L3.js
@@ -1,7 +1,9 @@
 //
-// 応用プログラミング 第12回 (ap12L1.js)
-// 
+// 応用プログラミング 第12回 (ap12L3.js)
 //
+// G18400-2021 拓殖太郎
+//
+
 "use strict"; // 厳格モード
 
 // ライブラリをモジュールとして読み込む
@@ -13,13 +15,13 @@ import * as L4 from "./ap12L4.js";
 
 let renderer;
 let camera;
-export const origin = new THREE.Vector3();
 let course;
+export const origin = new THREE.Vector3();
 export const controlPoints = [
     [-25,-40],
-    [ 50, 20],
+    [ 50, 20]
 ]
-export function init(scene, size, id, offset) {
+export function init(scene, size, id, offset, texture) {
     origin.set(offset.x, 0, offset.z);
     camera = new THREE.PerspectiveCamera(20, 1, 0.1, 1000);
     {
@@ -28,15 +30,12 @@ export function init(scene, size, id, offset) {
     }
     renderer =  new THREE.WebGLRenderer();
     {
-      renderer.setClearColor(0x808020);
+      renderer.setClearColor(0x406080);
       renderer.setPixelRatio(window.devicePixelRatio);
       renderer.setSize(size, size);
     }
     document.getElementById(id).appendChild(renderer.domElement);
-    // ポイントライト
-    const light = new THREE.PointLight();
-    light.position.set(offset.x, 2, offset.z);
-    scene.add(light);
+    
     // 平面
     const plane = new THREE.Mesh(
         new THREE.PlaneGeometry(100, 80),
@@ -45,79 +44,27 @@ export function init(scene, size, id, offset) {
     plane.rotateX(-Math.PI/2);
     plane.position.set(offset.x, -0.01, offset.z);
     scene.add(plane);
-    // コース
-    
-    const course = new THREE.CatmullRomCurve3(
-        controlPoints.map((p) => {
-            return (new THREE.Vector3()).set(
-                offset.x + p[0],
-                0,
-                offset.z + p[1]
-            );
-        }), false
-    )
-    const points = course.getPoints(100);
-    points.forEach((point)=>{
-        const road = new THREE.Mesh(
-            new THREE.CircleGeometry(5,16),
-            new THREE.MeshLambertMaterial({color: "gray"})
-        )
-        road.rotateX(-Math.PI/2);
-        road.position.set(
-            point.x,
-            0,
-            point.z
-        );
-        scene.add(road);
-    });
+
+    // ビル
+
+    // コース(描画)
 
 }
 
+// コース(自動運転用)
 export function makeCourse(scene) {
-    const courseVectors = [];
-    const parts = [L3, L4, L1, L2];
-    parts.forEach((part) => {
-        part.controlPoints.forEach((p) => {
-            courseVectors.push(
-                new THREE.Vector3(
-                    p[0] + part.origin.x,
-                    0,
-                    p[1] + part.origin.z,
-                )
-            )
-        });
-    })
-    course = new THREE.CatmullRomCurve3(
-        courseVectors, true
-    )
 }
 
+// カメラを返す
 export function getCamera() {
     return camera;
 }
 
+// 車の設定
 export function setCar(scene, car) {
-    const SCALE = 0.01;
-    car.position.copy(origin);
-    car.scale.set(SCALE, SCALE, SCALE);
-    car.rotation.y = 0;
-    car.visible = true;
-    scene.add(car);
 }
 
-const radius = 20;
-let angle = 0;
-const clock = new THREE.Clock();
-const carPosition = new THREE.Vector3();
-const carTarget = new THREE.Vector3();
 export function render(scene, car) {
-    const time = (clock.getElapsedTime() / 20);
-    angle = (angle + 0.02) % (2 * Math.PI);
-    course.getPointAt(time % 1, carPosition);
-    car.position.copy(carPosition);
-    course.getPointAt((time + 0.01) %1, carTarget);
-    car.lookAt(carTarget);
     camera.lookAt(car.position.x, car.position.y, car.position.z);
     renderer.render(scene, camera);
 }
-
